5494efc930071d842cc4dbc0572a22900448a646
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-04-25 21:48:33 +0300

    Convert env variables to uppercase

--- src/routes/oauth.js
+++ src/routes/oauth.js
@@ -25,7 +25,7 @@ router.post("/", async (req, res) => {
   if (code) {
     const params = new URLSearchParams();
     params.append("client_id", oauth.clientId);
-    params.append("client_secret", process.env.oauthclientsecret);
+    params.append("client_secret", process.env.OAUTH_CLIENT_SECRET);
     params.append("code", code);
     params.append("grant_type", "authorization_code");
     params.append("redirect_uri", oauth.redirectUri);
@@ -51,7 +51,7 @@ router.post("/", async (req, res) => {
 
   const accessToken = jwt.sign(
     { sub: data[oauth.jwt.identifier], iss: "nuc" },
-    process.env.jwtsecret,
+    process.env.JWT_SECRET,
     { expiresIn: "12h" }
   );
 
