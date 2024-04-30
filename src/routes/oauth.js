9d5368f3d51d31e16c4f2a635ac82e9262269bc5
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-04-30 16:22:12 +0300

    Add redirect_uri to oauth body

--- src/routes/oauth.js
+++ src/routes/oauth.js
@@ -8,10 +8,11 @@ const { oauth } = config();
 require("dotenv").config();
 
 router.post("/", async (req, res) => {
-  let { code, refreshToken } = Joi.attempt(
+  let { code, redirect_uri, refreshToken } = Joi.attempt(
     req.body,
     Joi.object({
       code: Joi.string().optional(),
+      redirect_uri: Joi.string().optional(),
       refreshToken: Joi.string().optional(),
     })
       .required()
@@ -22,13 +23,13 @@ router.post("/", async (req, res) => {
     return res.status(400).send("Missing OAuth Code and Refresh Token");
   }
 
-  if (code) {
+  if (code && redirect_uri) {
     const params = new URLSearchParams();
     params.append("client_id", oauth.clientId);
     params.append("client_secret", process.env.OAUTH_CLIENT_SECRET);
     params.append("code", code);
     params.append("grant_type", "authorization_code");
-    params.append("redirect_uri", oauth.redirectUri);
+    params.append("redirect_uri", redirect_uri);
 
     const { data } = await axios.post(oauth.tokenUrl, params.toString(), {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
