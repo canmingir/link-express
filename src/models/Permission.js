e7551c3cb3d96e6c5bd7c3b04118b300e0e26685
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-23 20:25:06 +0300

    Create Permission model

---
+++ src/models/Permission.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../postgres");
+
+const Permission = sequelize.define("Permission", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  itemId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  group: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Permission;
