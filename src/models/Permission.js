0b4e5fd9c98e2c9ca14e63beb045e1fd4afdea4d
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-24 17:22:01 +0300

    Add autoIncrement option to PermissionId

--- src/models/Permission.js
+++ src/models/Permission.js
@@ -6,6 +6,7 @@ const Permission = sequelize.define("Permission", {
     type: DataTypes.INTEGER,
     primaryKey: true,
     allowNull: false,
+    autoIncrement: true,
   },
   userId: {
     type: DataTypes.INTEGER,
