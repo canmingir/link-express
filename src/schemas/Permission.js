05c2c2541e6837a00ff17cb7515736ee6e855754
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-06-20 15:03:29 +0300

    Update joi schema according to new Permission

--- src/schemas/Permission.js
+++ src/schemas/Permission.js
@@ -3,12 +3,14 @@ const Joi = require("joi");
 module.exports = {
   create: Joi.object({
     appId: Joi.string().required(),
+    companyId: Joi.string().required(),
     projectId: Joi.string().guid().required(),
     userId: Joi.string().required(),
     role: Joi.string().required(),
   }).required(),
   list: Joi.object({
     appId: Joi.string().required(),
+    companyId: Joi.string().required(),
     projectId: Joi.string().guid().required(),
     userId: Joi.string().required(),
   }).required(),
