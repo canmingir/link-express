d2ba803649809e87d00912c13b33669b0050414a
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-06-26 12:48:59 +0300

    Create projects routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,6 @@ const metrics = require("./metrics");
 const permissions = require("./permissions");
 const oauth = require("./oauth");
 const companies = require("./companies");
+const projects = require("./projects");
 
-module.exports = { metrics, permissions, oauth, companies };
+module.exports = { metrics, permissions, oauth, companies, projects };
