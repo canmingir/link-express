ef0cb89befe7ca872b393f0d16f0f16756179100
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-06-26 12:49:30 +0300

    Add '/link' base to platform-express routes

--- src/express.js
+++ src/express.js
@@ -9,6 +9,7 @@ const app = express();
 const metrics = require("./routes/metrics");
 const permissions = require("./routes/permissions");
 const companies = require("./routes/companies");
+const projects = require("./routes/projects");
 const swaggerUi = require("swagger-ui-express");
 const openapi = require("./openapi");
 const error = require("./error");
@@ -40,8 +41,9 @@ app.use("/metrics", metrics);
 setImmediate(() => {
   process.env.PROFILE === "TEST" && app.use(authorization.verify);
 
-  app.use("/companies", companies);
-  app.use("/permissions", permissions);
+  app.use("/link/projects", projects);
+  app.use("/link/companies", companies);
+  app.use("/link/permissions", permissions);
   app.use((req, res) => res.status(404).end());
   app.use(error.handle);
 });
