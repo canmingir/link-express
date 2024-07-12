e78c43c247734ebd9a2f6c4ca98cd1bdc6195fc6
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-07-12 16:43:13 +0300

    Remove link from organizations spec

--- src/routes/test/organizations.spec.js
+++ src/routes/test/organizations.spec.js
@@ -6,22 +6,19 @@ const app = platform.express();
 const request = require("supertest");
 const { deepEqual } = require("assert");
 
-const { seed: organizations } = require("../../seeds/Organization");
-
 describe("Organization", () => {
   beforeEach(async () => {
     await test.reset();
   });
   it("lists organizations", async () => {
-    const { body: res } = await request(app)
-      .get("/link/organizations")
-      .expect(200);
+    const { body: res } = await request(app).get("/organizations").expect(200);
     deepEqual(res, organizations);
   });
   it("gets organization by id", async () => {
     const { body: res } = await request(app)
-      .get(`/link/organizations/${organizations[0].id}`)
+      .get(`/organizations/${organizations[0].id}`)
       .expect(200);
     deepEqual(res, organizations[0]);
   });
 });
+
