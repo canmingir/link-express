50ed575206ce8a1d917291a3565adba853f0ad39
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-07-22 14:19:59 +0300

    Add all permssion to test session

--- src/authorization.js
+++ src/authorization.js
@@ -7,7 +7,7 @@ function verify(req, res, next) {
       case "0c756054-2d28-4f87-9b12-8023a79136a5":
         req.session = {
           projectId: "0c756054-2d28-4f87-9b12-8023a79136a5",
-          userId: "marcus@nucleoidai.com",
+          userId: "1001",
           roles: ["ADMIN"],
           appId: "977f5f57-8936-4388-8eb0-00a512cf01cc",
           organizationId: "1c063446-7e78-432a-a273-34f481d0f0c3",
@@ -16,16 +16,34 @@ function verify(req, res, next) {
       case "21d2530b-4657-4ac0-b8cd-1a9f82786e32":
         req.session = {
           projectId: "21d2530b-4657-4ac0-b8cd-1a9f82786e32",
-          userId: "marcus@nucleoidai.com",
+          userId: "1001",
           roles: ["ADMIN"],
           appId: "977f5f57-8936-4388-8eb0-00a512cf01cc",
           organizationId: "5459ab03-204a-4627-bdde-667b7802cb35",
         };
         break;
+      case "add6dfa4-45ba-4da2-bc5c-5a529610b52f":
+        req.session = {
+          appId: "10b7bc8c-a49c-4002-b0ec-63599e4b5210",
+          organizationId: "dfb990bb-81dd-4584-82ce-050eb8f6a12f",
+          projectId: "add6dfa4-45ba-4da2-bc5c-5a529610b52f",
+          userId: "1001",
+          roles: ["ADMIN"],
+        };
+        break;
+      case "e6d4744d-a11b-4c75-acad-e24a02903729":
+        req.session = {
+          appId: "10b7bc8c-a49c-4002-b0ec-63599e4b5210",
+          organizationId: "1c063446-7e78-432a-a273-34f481d0f0c3",
+          projectId: "e6d4744d-a11b-4c75-acad-e24a02903729",
+          userId: "1001",
+          roles: ["ADMIN"],
+        };
+        break;
       default:
         req.session = {
           projectId: "cb16e069-6214-47f1-9922-1f7fe7629525",
-          userId: "marcus@nucleoidai.com",
+          userId: "1001",
           roles: ["ADMIN"],
           appId: "977f5f57-8936-4388-8eb0-00a512cf01cc",
           organizationId: "dfb990bb-81dd-4584-82ce-050eb8f6a12f",
@@ -67,5 +85,5 @@ function verify(req, res, next) {
 
   next();
 }
-
 module.exports = { verify };
+
