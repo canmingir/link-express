e5f3600cd4675fd41a9075f1d67bfa1986ccc53c
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-12-04 14:47:13 +0300

    Add condition to check organizationId is valid

--- src/routes/organizations.js
+++ src/routes/organizations.js
@@ -7,8 +7,14 @@ router.get("/", async (req, res) => {
   });
 });
 
-router.get("/:organizationId", async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { organizationId } = req.session;
+  const { id } = req.params;
+
+  if (organizationId !== id) {
+    return res.status(401).end();
+  }
+
   const organization = await Organization.findByPk(organizationId);
   res.status(200).json(organization);
 });
