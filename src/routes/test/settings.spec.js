ad35a52220abbb2d4650008a459ad25d460cace7
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-12-06 14:48:49 +0300

    Create settings tests

---
+++ src/routes/test/settings.spec.js
@@ -0,0 +1,47 @@
+const test = require("../../lib/test");
+
+const platform = require("../../platform");
+const app = platform.express();
+
+const request = require("supertest");
+const { deepEqual } = require("assert");
+const { log } = require("console");
+
+describe("Setting", () => {
+  beforeEach(async () => {
+    await test.reset();
+  });
+
+  it("lists settings", async () => {
+    const { body: res } = await request(app).get("/settings").expect(200);
+
+    deepEqual(res, [
+      {
+        id: "97e74ee6-8967-478d-bf53-d1da8daecb52",
+        teamId: "cb16e069-6214-47f1-9922-1f7fe7629525",
+        details: {
+          timeZone: "America/New_York",
+        },
+      },
+    ]);
+  });
+
+  it("updates settings", async () => {
+    const { body: res } = await request(app)
+      .patch("/settings")
+      .send({
+        details: {
+          timeZone: "America/Los_Angeles",
+        },
+      })
+      .expect(200);
+
+    deepEqual(res, {
+      id: "97e74ee6-8967-478d-bf53-d1da8daecb52",
+      teamId: "cb16e069-6214-47f1-9922-1f7fe7629525",
+      details: {
+        timeZone: "America/Los_Angeles",
+      },
+    });
+  });
+});
