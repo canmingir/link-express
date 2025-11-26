a8d82c638d87dd51742054d777683bb70524bfc5
Halil Cengel <halilibrahimcengel@icloud.com>
2025-11-26 13:45:03 +0300

    Init and create logger

---
+++ src/logger.js
@@ -0,0 +1,36 @@
+const { ecsFormat } = require("@elastic/ecs-pino-format");
+const pino = require("pino");
+const pinoElastic = require("pino-elasticsearch");
+
+const config = require("./config");
+
+const { logger: loggerConfig, project } = config();
+
+const streams = [{ stream: process.stdout }];
+
+if (loggerConfig && loggerConfig?.stream) {
+  const streamToElastic = pinoElastic({
+    index: loggerConfig.elasticsearch.index,
+    consistency: loggerConfig.elasticsearch.consistency || "one",
+    node: loggerConfig.elasticsearch.node,
+    esVersion: loggerConfig.elasticsearch.esVersion || 8,
+    flushBytes: loggerConfig.elasticsearch.flushBytes || 1000,
+  });
+
+  streams.push({ stream: streamToElastic });
+}
+
+const logger = pino(
+  {
+    ...ecsFormat(),
+    base: {
+      service: {
+        name: project.name,
+        version: project.version,
+      },
+    },
+  },
+  pino.multistream(streams)
+);
+
+module.exports = logger;
--- src/platform.js
+++ src/platform.js
@@ -5,6 +5,7 @@ const error = require("./error");
 let _express;
 let _postgres;
 let _dynamodb;
+let _logger;
 
 function init(config = {}) {
   return new Promise((resolve, reject) => {
@@ -13,9 +14,13 @@ function init(config = {}) {
         module.exports = fs.readFileSync(filename, "utf8").trim();
       };
 
-      const { postgres, dynamodb } = require("./config").init(config);
+      const { postgres, dynamodb, logger } = require("./config").init(config);
 
       _express = require("./express");
+
+      if (logger) {
+        _logger = require("./logger");
+      }
 
       if (postgres) {
         _postgres = require("./postgres");
@@ -43,4 +48,7 @@ module.exports = {
   require: (pkg) => require(pkg),
   authorization,
   error,
+  get logger() {
+    return _logger;
+  },
 };
