372e3ced0ce2d18c4d78ce150d30fe82ba36d66d
Halil Cengel <halilibrahimcengel@icloud.com>
2025-12-09 10:50:38 +0300

    Conver bin file and server file to js

---
+++ bin/cli.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const args = process.argv.slice(2);
+const command = args[0];
+
+function parseArgs(args) {
+  const options = {};
+  for (let i = 1; i < args.length; i++) {
+    if (args[i].startsWith("-p") || args[i].startsWith("--port")) {
+      options.port = args[i + 1];
+      i++;
+    }
+  }
+  return options;
+}
+
+if (command === "start") {
+  const options = parseArgs(args);
+  if (options.port) {
+    process.env.PORT = options.port;
+  }
+  require("../src/event/server/server.js");
+} else {
+  console.log("Usage: event-listener start [-p <port>]");
+  console.log("\nCommands:");
+  console.log("  start    Start the event server");
+  console.log("\nOptions:");
+  console.log("  -p, --port    Port to listen on (default: 8080)");
+  process.exit(1);
+}
---
+++ src/event/server/server.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+const { Server } = require("socket.io");
+const http = require("http");
+
+const server = http.createServer();
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+const subscriptions = {};
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("subscribe", (type) => {
+    if (!subscriptions[type]) subscriptions[type] = new Set();
+    subscriptions[type].add(socket.id);
+    console.log(`Socket ${socket.id} subscribed to ${type}`);
+  });
+
+  socket.on("unsubscribe", (type) => {
+    if (subscriptions[type]) {
+      subscriptions[type].delete(socket.id);
+      if (subscriptions[type].size === 0) delete subscriptions[type];
+      console.log(`Socket ${socket.id} unsubscribed from ${type}`);
+    }
+  });
+
+  socket.on("publish", ({ type, payload }) => {
+    console.log(`Publish: ${type}`, payload);
+    if (subscriptions[type]) {
+      subscriptions[type].forEach((sid) => {
+        if (sid !== socket.id) {
+          io.to(sid).emit("event", { type, payload });
+        }
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    Object.keys(subscriptions).forEach((type) => {
+      subscriptions[type].delete(socket.id);
+      if (subscriptions[type].size === 0) delete subscriptions[type];
+    });
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Event server listening on port ${PORT}`);
+});
